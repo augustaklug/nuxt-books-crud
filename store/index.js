caf3d4adf8c48230e9afa74db04d6c40e365d40f
augustaklug <56510700+augustaklug@users.noreply.github.com>
2020-03-22 17:19:35 -0300

    .

--- store/index.js
+++ store/index.js
@@ -1,5 +1,5 @@
 export const state = () => ({
-  products: [
+  movies: [
     {
       id: 1,
       name: 'Drill',
@@ -52,57 +52,3 @@ export const state = () => ({
     }
   ]
 })
-
-export const mutations = {
-  ADD_PRODUCT(state, product) {
-    console.log('add ' + product.name)
-    // get the id's of all products
-    const ids = state.products.map((prod) => prod.id)
-    // when there are no id's yet, 0 is the max.
-    ids.push(0)
-    // calculate new id
-    const newId = Math.max(...ids) + 1
-    state.products.push({
-      id: newId,
-      name: product.name,
-      description: product.description,
-      price: product.price
-    })
-  },
-  UPDATE_PRODUCT(state, product) {
-    const found = state.products.find((p) => p.id === product.id)
-    console.log('update ' + found.name)
-    found.name = product.name
-    found.description = product.description
-    found.price = product.price
-  },
-  DELETE_PRODUCT(state, product) {
-    const foundIndex = state.products.findIndex((p) => p.id === product.id)
-    console.log('delete ' + product.name)
-    if (foundIndex !== -1) state.products.splice(foundIndex, 1)
-  }
-}
-
-export const actions = {
-  // to wait for the result, return a Promise.
-  // You generally want this, if the (validation-) result decides the navigation.
-  // You don't have to wait if add-product and list is on the same page.
-  addProduct({ commit }, product) {
-    new Promise((resolve) => setTimeout(resolve, 2000)).then(() => {
-      console.log('add1')
-      commit('ADD_PRODUCT', product)
-    })
-  },
-  // just for the demo of nuxtServerInit
-  nuxtServerInit({ commit }, { req }) {
-    commit('ADD_PRODUCT', {
-      name: 'Wire cutters',
-      description: 'Cut the wire',
-      price: 40,
-      features: [
-        { id: 51, name: 'Handy' },
-        { id: 52, name: 'Tool' }
-      ]
-    })
-  }
-}
