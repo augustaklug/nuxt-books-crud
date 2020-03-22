c6353ad64cb880dc9df27e383e826e9f59b561ac
augustaklug <56510700+augustaklug@users.noreply.github.com>
2020-03-22 16:20:23 -0300

    Initial commit

---
+++ components/product-form.vue
@@ -0,0 +1,40 @@
+<template>
+  <span>
+    <div class="form-group">
+      <label for="edit-name">Name</label>
+      <input
+        id="edit-name"
+        v-model="theProduct.name"
+        class="form-control"
+        required
+      />
+    </div>
+    <div class="form-group">
+      <label for="edit-description">Description</label>
+      <textarea
+        id="edit-description"
+        v-model="theProduct.description"
+        class="form-control"
+        rows="3"
+      ></textarea>
+    </div>
+    <div class="form-group">
+      <label for="edit-price"
+        >Price <span class="glyphicon glyphicon-euro"></span
+      ></label>
+      <input
+        id="edit-price"
+        v-model="theProduct.price"
+        type="number"
+        class="form-control"
+      />
+    </div>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'ProductForm',
+  props: ['theProduct']
+}
+</script>
---
+++ pages/add-product.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>Add new product</h2>
+    <form @submit.prevent="createProduct">
+      <product-form :the-product="product"></product-form>
+      <button type="submit" class="btn btn-primary">Create</button>
+      <nuxt-link to="/" class="btn btn-default">Cancel</nuxt-link>
+    </form>
+  </section>
+</template>
+
+<script>
+import ProductForm from '../components/product-form.vue'
+
+export default {
+  components: { ProductForm },
+  layout: 'vue-crud',
+  data() {
+    return {
+      product: { id: null, name: '', description: '', price: '', features: [] }
+    }
+  },
+  methods: {
+    createProduct(e) {
+      // normally use store.commit, but as example, if REST backend is used, call store.dispatch action.
+      this.$store.dispatch('addProduct', this.product)
+      this.$router.push('/')
+    }
+  }
+}
+</script>
