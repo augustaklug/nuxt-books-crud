ff7b38bbdee47637dee9dbe0f4a138382e99c6cb
augustaklug <56510700+augustaklug@users.noreply.github.com>
2020-03-23 18:26:16 -0300

    heroku deploy

--- services/EventService.js
+++ services/EventService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:8081`,
+  baseURL: `https://crud-books-api.herokuapp.com/`,
   withCredentials: false,
   headers: {
     Accept: 'application/json',
